95aa10292a3e6e08a96915c464616c33978441c7
Sahil64242 <h20230084@hyderabad.bits-pilani.ac.in>
2024-07-26 23:01:30 +0530

    Add files via upload

---
+++ admin.h
@@ -0,0 +1,13 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "utils.h"
+
+#endif
