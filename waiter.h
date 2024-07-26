f0feced9a44f3a679735469c42a83cfd8fbed01f
Sahil64242 <h20230084@hyderabad.bits-pilani.ac.in>
2024-07-26 23:03:04 +0530

    Add files via upload

---
+++ waiter.h
@@ -0,0 +1,15 @@
+#ifndef WAITER_H
+#define WAITER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "utils.h"
+
+int findPriceOfMenuItem(int orderNumber);
+
+#endif
