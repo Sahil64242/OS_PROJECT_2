5bd6ebfb64f39e7c2e6ba618d159150e3b0715e6
Sahil64242 <h20230084@hyderabad.bits-pilani.ac.in>
2024-07-26 23:01:57 +0530

    Add files via upload

---
+++ manager.h
@@ -0,0 +1,13 @@
+#ifndef MANAGER_H
+#define MANAGER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "utils.h"
+
+#endif
